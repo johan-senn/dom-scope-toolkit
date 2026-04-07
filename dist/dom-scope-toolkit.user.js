6822646b2ac2e2c414373ae83dd04a245dfeac04
Johan Senn <johan.senn@hotmail.fr>
2026-04-07 08:18:29 +0200

    Ajout userscript minimal avec alerte

---
+++ dist/dom-scope-toolkit.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Dom Scope Toolkit
+// @namespace    https://github.com/johan-senn/dom-scope-toolkit
+// @version      0.1.0
+// @description  Toolkit d'exploration du DOM
+// @author       Johan Senn
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    alert('Dom Scope Toolkit actif');
+})();
