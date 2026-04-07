a98daa0b786800ca21bf041ae85968037aabbbf9
Johan Senn <johan.senn@hotmail.fr>
2026-04-07 11:16:47 +0200

    Ajout debug clavier pour analyse des touches pavé numerique

--- src/bootstrap/main.js
+++ src/bootstrap/main.js
@@ -7,8 +7,18 @@ import { alertTestModule } from '../modules/alert-test/index.js';
     registerModule(alertTestModule);
 
     document.addEventListener('keydown', (event) => {
+
+        console.log('DEBUG KEY:', {
+            key: event.key,
+            code: event.code,
+            ctrl: event.ctrlKey,
+            shift: event.shiftKey,
+            alt: event.altKey
+        });
+
         if (event.ctrlKey && event.shiftKey && event.code === 'Numpad0') {
             event.preventDefault();
+            console.log('NodeScope toggle déclenché');
             toggle();
         }
     });
